f69c1ec3bd4422c65782a6766262e85910071b8a
hooke007 <hooke007@qq.com>
2025-07-22 23:33:11 +0800

    着色器： kSGEDS_RT 改进边缘检测

--- portable_config/shaders/kSGEDS_RT.glsl
+++ portable_config/shaders/kSGEDS_RT.glsl
@@ -19,7 +19,13 @@ LICENSE:
 //!TYPE float
 //!MINIMUM 0.0
 //!MAXIMUM 255.0
-16.0
+64.0
+
+//!PARAM ECS
+//!TYPE float
+//!MINIMUM 1.0
+//!MAXIMUM 50.0
+10.0
 
 
 //!HOOK SCALED
@@ -32,19 +38,20 @@ vec4 hook() {
 	vec2 pos = HOOKED_pos;
 	vec2 pt = HOOKED_pt;
 	vec4 color = HOOKED_tex(pos);
-	vec4 posl;
-	posl.x = HOOKED_texOff(vec2(1.0, 1.0)).g;
-	posl.y = HOOKED_texOff(vec2(1.0, 0.0)).g;
-	posl.z = HOOKED_texOff(vec2(0.0, 0.0)).g;
-	posl.w = HOOKED_texOff(vec2(0.0, 1.0)).g;
-	float edgeVote = abs(posl.z - posl.y) + abs(color.g - posl.y) + abs(color.g - posl.z);
 
-	if (edgeVote > (ET / 255.0)) {
-		vec4 pix_l = HOOKED_tex(pos + vec2(-pt.x, 0));
-		vec4 pix_r = HOOKED_tex(pos + vec2(pt.x, 0));
-		vec4 pix_u = HOOKED_tex(pos + vec2(0, -pt.y));
-		vec4 pix_d = HOOKED_tex(pos + vec2(0, pt.y));
-		float laplacian_g = 4.0 * color.g - (pix_l.g + pix_r.g + pix_u.g + pix_d.g);
+	vec4 pix_l = HOOKED_tex(pos + vec2(-pt.x, 0.0));
+	vec4 pix_r = HOOKED_tex(pos + vec2( pt.x, 0.0));
+	vec4 pix_u = HOOKED_tex(pos + vec2(0.0, -pt.y));
+	vec4 pix_d = HOOKED_tex(pos + vec2(0.0,  pt.y));
+
+	float laplacian_g = 4.0 * color.g - (pix_l.g + pix_r.g + pix_u.g + pix_d.g);
+	float local_clearness_raw = abs(laplacian_g);
+
+	float clearness_factor = clamp(local_clearness_raw * ECS, 0.0, 1.0);
+	float dynamic_ET = (ET / 255.0) * clearness_factor;
+	float edgeVote = abs(pix_l.g - pix_r.g) + abs(pix_u.g - pix_d.g);
+
+	if (edgeVote > dynamic_ET) {
 		float deltaY = SHARP * laplacian_g;
 		deltaY = clamp(deltaY, -23.0 / 255.0, 23.0 / 255.0);
 		color.rgb += vec3(deltaY);
